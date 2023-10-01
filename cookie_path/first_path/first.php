08bc29f36290c1c9bf497c909480ee8c7420dacf
ahmedgalal8540@gmail.com <ahmed.galal8540@gmail.com>
2023-10-01 08:33:57 -0400

    Tests for cookie path

---
+++ cookie_path/first_path/first.php
@@ -0,0 +1,15 @@
+<?php
+setcookie("first_cookie","This is first cookie",time()+3600,"/first_path");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First file</title>
+</head>
+<body>
+    <p>This is the first path</p>
+</body>
+</html>
